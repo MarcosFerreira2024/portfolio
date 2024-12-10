1568bd2b42f6bb2b26ae043c3493e0a05854f869
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-12-10 03:22:42 -0300

    no used vars

--- src/app/components/BotaoMenor.tsx
+++ src/app/components/BotaoMenor.tsx
@@ -12,7 +12,7 @@ interface Ibotao extends React.AnchorHTMLAttributes<HTMLAnchorElement>{
 function BotaoMenor({href,width=18,height=18,classes,src,alt,...props}:Ibotao) {
   return (
     <>
-      <Link href={href} target='_blank' className='w-[39px] h-[39px] group border-title border-[1.5px] rounded-[7.5px] flex items-center justify-center hover:bg-purple-700 hover:text-white transition-colors ease-linear duration-500' {...props} ><Image className='group-hover:dark:invert group-hover:dark:brightness-0 transition-colors ease-linear duration-300' src={src} width={width} height={height} alt={alt} /></Link>
+      <Link href={href} target='_blank' className= {`${classes} w-[39px] h-[39px] group border-title border-[1.5px] rounded-[7.5px] flex items-center justify-center hover:bg-purple-700 hover:text-white transition-colors ease-linear duration-500`} {...props} ><Image className='group-hover:dark:invert group-hover:dark:brightness-0 transition-colors ease-linear duration-300' src={src} width={width} height={height} alt={alt} /></Link>
     </>
   )
 }
--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,8 +1,6 @@
 "use client"
 import Image from 'next/image'
-import Link from 'next/link'
 import React from 'react'
-import Botao from './BotaoMaior'
 import BotaoMaior from './BotaoMaior'
 import BotaoMenor from './BotaoMenor'
 
