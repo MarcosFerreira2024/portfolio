6ac8342c075b88937addff4715fb7b2dfb1b130f
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-12-10 21:53:56 -0300

    skills animation, svgs e data

---
+++ src/app/data/skills.ts
@@ -0,0 +1,47 @@
+export const skills = [
+    {
+        nome: 'NextJs',
+        iconPath: '/Main/nextjs.svg',
+        alt: 'Ícone NextJs',
+    },
+    {
+        nome: 'React',
+        iconPath: '/Main/react.svg',
+        alt: 'Ícone React',
+    },
+    {
+        nome: 'TypeScript',
+        iconPath: '/Main/typescript.svg',
+        alt: 'Ícone TypeScript',
+    },
+    {
+        nome: 'JavaScript',
+        iconPath: '/Main/javascript.svg',
+        alt: 'Ícone JavaScript',
+    },
+    {
+        nome: 'TailwindCSS',
+        iconPath: '/Main/tailwindcss.svg',
+        alt: 'Ícone TailwindCSS',
+    },
+    {
+        nome: 'CSS',
+        iconPath: '/Main/css.svg',
+        alt: 'Ícone CSS',
+    },
+    {
+        nome: 'HTML',
+        iconPath: '/Main/html.svg',
+        alt: 'Ícone HTML',
+    },
+    {
+        nome: 'Figma',
+        iconPath: '/Main/figma.svg',
+        alt: 'Ícone Figma',
+    },
+    {
+        nome: 'Python',
+        iconPath: '/Main/python.svg',
+        alt: 'Ícone Python',
+    },
+];
