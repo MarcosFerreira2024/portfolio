c0463330c998b2402ff2241c03ca247ee32e4624
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-06 05:06:02 -0300

    dando fetch na api do backend

--- src/app/utils/fetch.ts
+++ src/app/utils/fetch.ts
@@ -1,11 +1,12 @@
 import { Certificados, Projetos } from "@/app/types/apiTypes"
 
-export const getProjetos =  async () =>{
+const host = "https://back-end-portfolio-marcosferreira2024s-projects.vercel.app/api"
+export const getProjetos = async () => {
     try {
-        const response = await fetch("http://localhost:3000/projetos")
+        const response = await fetch(`${host}/projetos`)
         const data = await response.json()
         return data as Projetos[]
-    }catch{
+    } catch {
         return null
     }
 
@@ -16,10 +17,10 @@ export const getProjetos =  async () =>{
 
 export const getCertificados = async () => {
     try {
-        const response = await fetch("http://localhost:3000/certificados")
-        const data = await response.json() 
+        const response = await fetch(`${host}/certificados`)
+        const data = await response.json()
         return data as Certificados[]
-    }catch{
+    } catch {
         return null
     }
 }
