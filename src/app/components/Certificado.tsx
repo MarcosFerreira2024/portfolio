a5157d42f58b8b1252fb08a6c0e1e441004217fa
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-07 05:19:27 -0300

    pegando dados do local storage

--- src/app/components/Certificado.tsx
+++ src/app/components/Certificado.tsx
@@ -1,8 +1,26 @@
+"use client"
+import { useEffect, useState } from "react";
 import { getCertificados } from "../utils/fetch";
 import BotaoMaior from "./BotaoMaior";
+import Loading from "./Loading";
+import { Certificados } from "../types/apiTypes";
 
-async function Certificado() {
-  const data = await getCertificados();
+function Certificado() {
+  const[data,setData] = useState<null|Certificados[]>(null)
+    useEffect(()=>{
+        if(localStorage.getItem("dataCertificados")){
+            setData(JSON.parse(localStorage.getItem("dataCertificados") as string))
+            return
+        }
+            async function fetchData(){
+                const data = await getCertificados()
+                if(data){
+                    localStorage.setItem("dataCertificados",JSON.stringify(data))
+                    setData(data)
+                }
+            }
+            fetchData()
+    },[])
 
   return (
     <section className="grid justify-center gap-5 md:gap-0 md:grid-cols-2 ">
@@ -16,7 +34,7 @@ async function Certificado() {
                 <div className=" h-[2px] max-w-[322px] mt-2 bg-subtitle"></div>
             </div>
         </div>
-      )):null}
+      )):<Loading/>}
     </section>
   );
 }
