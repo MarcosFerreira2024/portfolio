5c95b0de908065526f002dc32c1856f00f1225e2
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-01-09 05:04:46 -0300

    componente botão contato

--- src/app/components/Contato.tsx
+++ src/app/components/Contato.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import BotaoMenor from './BotaoMenor'
-import Link from 'next/link'
 import Image from 'next/image'
 import BotaoMaior from './BotaoMaior'
+import BotaoContato from './BotaoContato'
 
 function Contato() {
     const zap = 'https://wa.me/5521982752636'
@@ -16,34 +15,14 @@ function Contato() {
     <div className='m-5 md:m-0 border-2 border-title rounded-[7.5px]'>
         <div className='grid justify-center md:grid-cols-2'>
             <div className='p-10 flex flex-col gap-5'>
-                <div className='flex items-center gap-2 animaContato '>
-                    <BotaoMenor alt='meu telefone' href={zap} target='_blank' width={18} height={18} src='/Main/telefone.svg'/>
-                    <Link href={zap} target='_blank' className='text-subtitle font-title dark:hover:text-purple-500 hover:text-purple-800 transition-colors duration-150 ease-in-out'>(21) 98275-2636</Link>
-                </div>
-
-                <div className='flex items-center gap-2 animaContato'>
-                    <BotaoMenor alt='meu github' href='https://www.github.com/MarcosFerreira2024' target='_blank' width={18} height={18} src='/Main/github.svg'/>
-                    <Link href={github} target='_blank' className='text-subtitle font-title dark:hover:text-purple-500 hover:text-purple-800 transition-colors duration-150 ease-in-out'>MarcosFerreira2024</Link>
-                </div>
-
-                <div className='flex items-center gap-2 animaContato'>
-                    <BotaoMenor alt='meu email' href='mailto:marcosfp2021@hotmail.com?subject=Ola%20Marcos,%20Gostaria%20de%20entrar%20em%20contato%20para%20oferecer%20uma%20oportunidade%20de%20emprego&body=' target='_blank' width={18} height={18} src='/Main/email.svg'/>
-                    <Link href={email} target='_blank' className='text-subtitle font-title dark:hover:text-purple-500 hover:text-purple-800 transition-colors duration-150 ease-in-out'>marcosfp2021@hotmail.com</Link>
-                </div>
-
-                <div className='flex items-center gap-2 animaContato'>
-                    <BotaoMenor alt='meu linkedin' href='https://www.linkedin.com/in/marcos-vinicius-ferreira/' target='_blank' width={18} height={18} src='/Main/linkedin.svg'/>
-                    <Link href={linkedin} target='_blank' className='text-subtitle font-title dark:hover:text-purple-500 hover:text-purple-800 transition-colors duration-150 ease-in-out'>Marcos-Vinicius-Ferreira</Link>
-                </div>
-
-                <div className='flex items-center gap-2 animaContato'>
-                    <BotaoMenor alt='Rio De Janeiro' href={local}   width={18} height={18} src='/Main/local.svg'/>
-                    <Link href={local} target='_blank'  className='text-subtitle font-title dark:hover:text-purple-500 hover:text-purple-800 transition-colors duration-150 ease-in-out'>Rio de Janeiro - Brasil</Link>
-                </div>
-                <div className='md:hidden flex'>
-                    <BotaoMaior href={curriculo} target="_blank" classes="w-[206px] justify-between" label="Baixar Currículo" src={`/Main/download.svg`} alt="download"/>
-                </div>
-
+                <h1 className='text-subtitle group  font-title'>
+                    Minhas informações ↓
+                </h1>
+                    <BotaoContato src='/Main/telefone.svg' alt='Meu zap' href={zap} info='(21)9275-2636'  />
+                    <BotaoContato src='/Main/github.svg' alt='Meu github' href={github} info='Marcos Ferreira 2024'  />
+                    <BotaoContato src='/Main/email.svg' alt='Meu email' href={email} info='marcosfp2021@hotmail.com'  />
+                    <BotaoContato src='/Main/linkedin.svg' alt='Meu linkedin' href={linkedin} info='Marcos-Vinicius-Ferreira'  />
+                    <BotaoContato src='/Main/local.svg' alt='Local Desejado' href={local} info='Rio de Janeiro - Brasil'  />
             </div>
             <div className='md:flex hidden flex-col  items-center justify-between gap-5 p-10'>
                 <div className='flex flex-col gap-2 items-center '>
